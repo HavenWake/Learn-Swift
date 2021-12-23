01d62884aad888c5b6c2cbc3f4bf965f143a6583
HaveN <96118834+HavenWake@users.noreply.github.com>
2021-12-23 19:51:22 +0300

    update

--- PryanichnikovIOSPDC-3_HW2_2.playground/Contents.swift
+++ PryanichnikovIOSPDC-3_HW2_2.playground/Contents.swift
@@ -1,65 +1,118 @@
 import UIKit
+import Darwin
+import Foundation
 //Задача 1
-let salaryWorkerOne = 110000
-let salaryWorkerTwo = 55000
-let salaryWorkerThree = 70000
+let january = "Январь"
+let february = "Февраль"
+let march = "Март"
+let april = "Апрель"
+let may = "Май"
+let june = "Июнь"
+let july = "Июль"
+let august = "Август"
+let september = "Сентябрь"
+let october = "Октябрь"
+let november = "Ноябрь"
+let december = "Декабрь"
+var selectedMonth: String
 
-let matchOne = salaryWorkerOne > salaryWorkerThree
-print(matchOne)
-let matchTwo = salaryWorkerTwo < salaryWorkerOne
-print(matchTwo)
-let matchThree = salaryWorkerOne > salaryWorkerTwo || salaryWorkerThree < salaryWorkerTwo
-print(matchThree)
-let matchFour = salaryWorkerThree < salaryWorkerOne && salaryWorkerTwo > salaryWorkerOne
-print(matchFour)
+selectedMonth = may
+
+if selectedMonth == "Январь" {
+    print("Текущий месяц Январь, кол-во рабочих дней: \(31 - (8+8))")
+}
+else if selectedMonth == "Февраль" {
+    print("Текущий месяц Февраль, кол-во рабочих дней: \(28 - (8+1))")
+}
+else if selectedMonth == "Март" {
+    print("Текущий месяц Март, кол-во рабочих дней: \(31 - (8+1))")
+}
+else if selectedMonth == "Апрель" {
+    print("Текущий месяц Апрель, кол-во рабочих дней: \(30 - (8))")
+}
+else if selectedMonth == "Май" {
+    print("Текущий месяц Май, кол-во рабочих дней: \(31 - (8+1))")
+}
+else if selectedMonth == "Июнь" {
+    print("Текущий месяц Июнь, кол-во рабочих дней: \(30 - (8+1))")
+}
+else if selectedMonth == "Июль" {
+    print("Текущий месяц Июль, кол-во рабочих дней: \(31 - (8))")
+}
+else if selectedMonth == "Август" {
+    print("Текущий месяц Август, кол-во рабочих дней: \(31 - (8))")
+}
+else if selectedMonth == "Сентябрь" {
+    print("Текущий месяц Сентябрь, кол-во рабочих дней: \(30 - (8))")
+}
+else if selectedMonth == "Октябрь" {
+    print("Текущий месяц Октябрь, кол-во рабочих дней: \(31 - (8))")
+}
+else if selectedMonth == "Ноябрь" {
+    print("Текущий месяц Ноябрь, кол-во рабочих дней: \(30 - (8+1))")
+}
+else if selectedMonth == "Декабрь" {
+    print("Текущий месяц Декабрь, кол-во рабочих дней: \(31 - (8))")
+}
 
 //Задача 2
-
-let a = 0
-let b = 0
-let c = 0
-
-let d = b * b - 4 * a * c
-
-//Через if-else
-if d > 0 {
-    print("График скорее всего продолжит направление")
-}
-else if d < 0 {
-    print("График скорее всего поменяет направление")
-}
-else {
-    print("Неопределенная ситуация - нужно ждать")
-}
-
-//Через ?
-d > 0 ? print("График скорее всего продолжит направление") :  d == 0 ? print("Неопределенная ситуация - нужно ждать") : print("График скорее всего поменяет направление")
-
-//Через Switch
-switch d {
-case 1...:
-    print("График скорее всего продолжит направление")
-case ..<0:
-    print("График скорее всего поменяет направление")
+switch selectedMonth {
+case "Январь":
+    print("Текущий месяц Январь, кол-во рабочих дней: \(31 - (8+8))")
+case "Февраль":
+    print("Текущий месяц Февраль, кол-во рабочих дней: \(28 - (8+1))")
+case "Март":
+    print("Текущий месяц Март, кол-во рабочих дней: \(31 - (8+1))")
+case "Апрель":
+    print("Текущий месяц Апрель, кол-во рабочих дней: \(30 - (8))")
+case "Май":
+    print("Текущий месяц Май, кол-во рабочих дней: \(31 - (8+1))")
+case "Июнь":
+    print("Текущий месяц Июнь, кол-во рабочих дней: \(30 - (8+1))")
+case "Июль":
+    print("Текущий месяц Июль, кол-во рабочих дней: \(31 - (8))")
+case "Август":
+    print("Текущий месяц Август, кол-во рабочих дней: \(31 - (8))")
+case "Сентябрь":
+    print("Текущий месяц Сентябрь, кол-во рабочих дней: \(30 - (8))")
+case "Октябрь":
+    print("Текущий месяц Октябрь, кол-во рабочих дней: \(31 - (8))")
+case "Ноябрь":
+    print("Текущий месяц Ноябрь, кол-во рабочих дней: \(30 - (8+1))")
+case "Декабрь":
+    print("Текущий месяц Декабрь, кол-во рабочих дней: \(31 - (8))")
 default:
-    print("Неопределенная ситуация - нужно ждать")
+    print("Ошибка, месяц не определен")
 }
-
 
 //Задача 3
-var yearsOld = 25
-let gender = "male"
+var isHoliday = false
 
-if yearsOld >= 3 && yearsOld < 17
-{
-    gender == "male" ? print("Детский контент для мальчиков") : gender == "female" ? print("Детский контент для девочек") : print("Нет подходящего контента")
+isHoliday == true ? print ("Сегодня выходной день") : print ("Сегодня рабочий день")
+
+//Задача 4
+enum Month {
+    case january, february, march, april, may, june, july, august, september, october, november, december
+
+    var description: String {
+        switch self {
+        case .january: return "Январь"
+        case .february: return "Февраль"
+        case .march: return "Март"
+        case .april: return "Апрель"
+        case .may: return "Май"
+        case .june: return "Июнь"
+        case .july: return "Июль"
+        case .august: return "Август"
+        case .september: return "Сентябрь"
+        case .october: return "Октябрь"
+        case .november: return "Ноябрь"
+        case .december: return "Декабрь"
+        }
+    }
 }
-else if yearsOld >= 18 && yearsOld <= 65{
-    gender == "male" ? print("Взрослый контент для мужчин") : gender == "female" ? print("Взрослый контент для женщин") : print("Нет подходящего контента")
-}
-else if yearsOld > 65 {
-    gender == "male" ? print("Контент для мужчин пожилого возраста") : gender == "female" ? print("Контент для женщин пожилого возраста") : print("Нет подходящего контента")
-}
-else{
-    print("Нет подходящего контента")
-}
+
+print(Month.december.description)
+
+
+
